138b9743dd99d687883859ae124043ad2d786522
aatif <aa@aatif.com>
2025-08-21 19:30:03 +0530

    V2

--- F/src/App.jsx
+++ F/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   useEffect(() => {
 
     const fetchData = async () => {
-      const response = await axios.get('http://localhost:3000/api/products')
+      const response = await axios.get('https://frontback-tufv.onrender.com/api/products')
       setProducts(response.data)
     }
     fetchData();
